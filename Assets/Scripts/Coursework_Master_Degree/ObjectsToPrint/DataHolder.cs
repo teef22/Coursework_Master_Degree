a1a1c54a012a6eb6e1ed35a192d697c930881413
Tonny <dimb0r22@gmail.com>
2026-06-10 18:42:52 +0300

    feat(new references): add mesh renderers and corners references

--- Assets/Scripts/Coursework_Master_Degree/ObjectsToPrint/DataHolder.cs
+++ Assets/Scripts/Coursework_Master_Degree/ObjectsToPrint/DataHolder.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using Coursework_Master_Degree.ScriptableObjects.ObjectsToPrint;
 
 namespace Coursework_Master_Degree.ObjectsToPrint
@@ -6,5 +7,7 @@ namespace Coursework_Master_Degree.ObjectsToPrint
     public class DataHolder : MonoBehaviour
     {
         public ObjectToPrintSO ObjectToPrintData;
+        public List<MeshRenderer> MeshRenderersList;
+        public List<GameObject> CornersList;
     }
 }
